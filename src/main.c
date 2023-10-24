72947d6703358fd9e72dea0dc2208626dc61e624
Pio Giacomo Cimino <piogiacomo.cimino@studenti.univr.it>
2023-10-24 01:16:59 -0700

    changed

---
+++ src/main.c
@@ -0,0 +1,28 @@
+/**
+ * @file main.cc
+ *
+ * @brief Permutation generator
+ *
+ * @author Pio Giacomo Cimino
+ *
+ */
+
+#include <perms.h>
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter the number of elements in the list: ");
+    scanf("%d", &n);
+
+    int list[n];
+    for (int i = 0; i < n; i++) {
+        list[i]=i;
+    }
+
+    printf("Permutations:\n");
+    generatePermutations(list, 0, n-1);
+
+    return 0;
+}
+
